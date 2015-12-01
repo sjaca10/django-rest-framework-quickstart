27236d333f06b97c70ebbee14f8e77f1945a08a7
Javier Aguila <sjaca10@gmail.com>
2015-12-01 15:10:55 -0600

    Quickstart completed

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from rest_framework import routers
+from tutorial.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing
+# Additionally, we include login URLs for the browsable API
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth', include('rest_framework.urls', namespace = 'rest_framework')),
 ]
